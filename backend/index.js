ad246436e3200f1deb4678da66dac1f05e94a5d1
itsShlok <96978009+itsShlok@users.noreply.github.com>
2024-01-01 21:23:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,19 +12,7 @@ app.use(express.urlencoded({extended:false}))
 app.use('/api',require('./routes/workshop_details.js'))
 app.use('/api',require('./routes/users.js'))
 
-// app.post("/workshop-details", async (req, res) => {
-//     try {
-//       const { workshop_name } = req.body;
-//       const newTodo = await pool.query(
-//         "INSERT INTO ws_detail_update (workshop_name) VALUES($1) RETURNING *",
-//         [workshop_name]
-//       );
-//   
-//       res.json(newTodo.rows[0]);
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   });
+
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
